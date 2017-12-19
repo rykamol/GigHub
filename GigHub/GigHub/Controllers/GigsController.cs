9553652e7a5359245290b7fdf101fb7b50171ac4
Kamol chandra roy <kamol.roy@outlook.com>
2017-12-19 17:00:33 +0000

    Create Add a Gig form. Imported from Changeset 12

---
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using GigHub.Models;
+using GigHub.ViewModles;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        public ApplicationDbContext Context;
+
+
+        public GigsController()
+        {
+            Context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = Context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ GigHub/GigHub/ViewModles/GigFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModles
+{
+    public class GigFormViewModel
+    {
+        public string Vanue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
