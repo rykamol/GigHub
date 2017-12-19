0011a92941bd4515789902d1db333436b1a2cc39
Kamol chandra roy <kamol.roy@outlook.com>
2017-12-19 10:04:42 +0000

    Create the gig model Imported from Changeset 10

---
+++ GigHub/GigHub/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; } //255 max
+        
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+    }
+}
